5c6febf04d73f25c322fbdaf63b2d92eb9f3fdbd
Moses <mosesletting19@gmail.com>
2023-12-17 04:30:39 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+}
+
+
+
+function countDown(startingNumber) {
+    if (Number.isInteger(startingNumber) && startingNumber > 0) {
+        while (startingNumber >= 0) {
+            console.log(startingNumber);
+            startingNumber--;
+        }
+    } else {
+        console.log('Please provide a positive integer as input.');
+    }
+}
